2cdaf126cd133ad47424a02a3201b29ded2713ef
shamik <shamik.miscrits@gmail.com>
2024-04-03 22:08:22 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import Results from '@/components/Results';
 
-function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,{next: {revalidate:10000}}
+  );
+  const data = await res.json();
+  console.log(data.results[0])
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+
   return (
     <div>
-      
+      <Results results={results} />
     </div>
-  )
+  );
 }
-
-export default Home
