ec3c0a3c0cc6fd874f7e6db42fdb6efa9daae734
shamik <shamik.miscrits@gmail.com>
2024-04-03 22:49:29 +0530

    latest

--- src/components/NavBarItem.jsx
+++ src/components/NavBarItem.jsx
@@ -1,24 +1,23 @@
-'use client'
-import Link from 'next/link'
-import React from 'react'
-import { useSearchParams } from 'next/navigation'
+'use client';
 
-function NavBarItem({title,param}) {
-    const searchParams=useSearchParams()
-    const genre=searchParams.get('genre')
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
   return (
     <div>
-      <Link href={`/?genre=${param}`}
-      className={`hover:text-purple-600 font-semibold ${
-        genre === param
-          ? 'underline underline-offset-8 decoration-4 decoration-purple-500 rounded-lg'
-          : ''
-      }`}
+      <Link
+        className={`hover:text-purple-400 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-purple-400 rounded-lg'
+            : ''
+        }`}
+        href={`/?genre=${param}`}
       >
-      {title}
+        {title}
       </Link>
     </div>
-  )
+  );
 }
-
-export default NavBarItem
