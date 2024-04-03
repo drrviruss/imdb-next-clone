91826d09f777f35492072fca9ff425545b06af36
shamik <shamik.miscrits@gmail.com>
2024-04-03 22:55:38 +0530

    suspense

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBarItem from './NavBarItem'
+import { Suspense } from 'react'
 
 function NavBar() {
   return (
     <div className='flex dark:bg-gray-700 bg-purple-200 p-4 lg:text-lg justify-center gap-6'>
+      <Suspense>
       <NavBarItem title="Trending" param="fetchTrending"/>
       <NavBarItem title="Top Rated" param="fetchTopRated"/>
+
+      </Suspense>
+      
     </div>
   )
 }
